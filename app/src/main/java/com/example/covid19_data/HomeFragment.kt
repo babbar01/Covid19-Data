a2863719b99818a347277d8549856c270fcccdcc
babbar01 <53184162+babbar01@users.noreply.github.com>
2020-10-07 20:44:13 +0530

    new card added in home fragment for previous day data

--- app/src/main/java/com/example/covid19_data/HomeFragment.kt
+++ app/src/main/java/com/example/covid19_data/HomeFragment.kt
@@ -2,21 +2,25 @@ package com.example.covid19_data
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import kotlinx.android.extensions.LayoutContainer
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
 
-    companion object{
+    companion object {
         val TAG = "mytag"
     }
 
@@ -36,14 +40,76 @@ class HomeFragment : Fragment() {
 
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
-                tvtotalCase.setText(response.getString("totalCases"))
-                tvactiveCase.setText(response.getString("activeCases"))
-                tvdeathCase.setText(response.getString("deaths"))
-                tvdischargedCase.setText(response.getString("recovered"))
+                val confirmedCases = response.getString("totalCases").toInt()
+                val activeCases = response.getString("activeCases")
+                val deathCases = response.getString("deaths").toInt()
+                val recoveredCases = response.getString("recovered").toInt()
+                val newActive = response.getString("activeCasesNew").toInt()
+                val newRecovered = response.getString("recoveredNew").toInt()
+                val newDeaths = response.getString("deathsNew").toInt()
+                val previousDayTest = response.getString("previousDayTests").toInt()
+
+                txt_total.text = confirmedCases.toString()
+                txt_deaths.text = deathCases.toString()
+                txt_recovered.text = recoveredCases.toString()
+                txt_newActive.text = newActive.toString()
+                txt_newRecovered.text = newRecovered.toString()
+                txt_newDeaths.text = newDeaths.toString()
+                txt_previousDayTests.text = previousDayTest.toString()
+
+                // setting up pie chart
+
+                val entries: MutableList<PieEntry> = ArrayList()
+
+                entries.add(PieEntry(confirmedCases.toFloat(), "Confirmed"))
+                entries.add(PieEntry(deathCases.toFloat(), "Deaths"))
+                entries.add(PieEntry(recoveredCases.toFloat(), "Recovered"))
+
+//                val colorsArray = arrayOf<Int>(
+//                    R.color.color_confirmed,
+//                    R.color.color_death,
+//                    R.color.color_recovered
+//                )
+
+                val set = PieDataSet(entries, "Corona Cases")
+                set.setDrawValues(false)
+                if (container != null) {
+                    set.setColors(
+                        ContextCompat.getColor(container.context, R.color.color_confirmed),
+                        ContextCompat.getColor(container.context,  R.color.color_death),
+                        ContextCompat.getColor(container.context, R.color.color_recovered)
+                    )
+                }
+                val data = PieData(set)
+
+                pieChart.legend
+                pieChart.apply {
+                    this.data = data
+                    setDrawEntryLabels(false)
+                    holeRadius = 70f
+                    centerText = "$activeCases Cases Active"
+                    if (container != null) {
+                        setCenterTextColor(ContextCompat.getColor(container.context,R.color.pieCenterText))
+                        setHoleColor(ContextCompat.getColor(container.context,R.color.card_grey))
+                    }
+
+                    legend.isEnabled = false
+                    setDrawEntryLabels(false)
+                    description.isEnabled = false
+
+                    animateXY(1000,1000)
+                }
+
 
                 progress_home.visibility = View.GONE
                 homeContainer?.visibility = View.VISIBLE
                 Log.d(TAG, "onCreateView: response success")
+
+
+                pieChart.invalidate()
+
+
+
             },
             Response.ErrorListener { error ->
 //                Toast.makeText(this,error.message,Toast.LENGTH_SHORT)
